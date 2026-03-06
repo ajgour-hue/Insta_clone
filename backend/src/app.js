47052a06ab084e7d0a4a81f982d0804f5af34489
Ajay Gour <theajaygour@gmail.com>
2026-03-06 20:41:27 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
 const authRouter = require("./routes/authRouter")
+const postRouter = require("./routes/postRouter")
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth" , authRouter)
+app.use("/api/posts" ,postRouter)
 module.exports = app;
