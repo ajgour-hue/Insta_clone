e6a83a9bb0439e2032b6ff477490a1162f22d090
Ajay Gour <theajaygour@gmail.com>
2026-03-11 20:44:27 +0530

    postapi

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,10 +5,22 @@ const userRouter =express.Router()
 const IdentifyUser = require("../middlewares/auth.middleware")
 
 
-//  @route POST /api/users/follow/:userId
-//   @description follow a user
-// @acess private
 
-userRouter.post("/follow/:username",IdentifyUser,userController.followUserController)
+/**
+ * @route POST /api/users/follow/:userid
+ * @description Follow a user
+ * @access Private
+ */
+userRouter.post("/follow/:username", IdentifyUser, userController.followUserController)
+
+
+/** 
+ * @route POST /api/users/unfollow/:userid
+ * @description Unfollow a user
+ * @access Private
+ */
+userRouter.post("/unfollow/:username", IdentifyUser, userController.unfollowUserController)
+
+
 
 module.exports = userRouter;
