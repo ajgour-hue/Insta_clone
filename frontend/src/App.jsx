0f9b281b3185ddeb15424714161cfdd105e78a93
Ajay Gour <theajaygour@gmail.com>
2026-03-13 23:00:23 +0530

    using layers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 
 import AppRoutes from './AppRoutes'
+import { AuthProvider } from './features/auth/auth.context.jsx'
 import "./style.scss"
 const App = () => {
   return (
     <div>
+     <AuthProvider>
      <AppRoutes />
+     </AuthProvider>
     </div>
   )
 }
